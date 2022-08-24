b22d68b3d920b76830f0bddd063f38bf521cb4cf
Aiden Bostwick <aidenbostwick5@gmail.com>
2022-08-24 17:20:31 -0400

    cart lab compleated

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+//const { product } = required("puppeteer");
 
-  //... your code goes here
+function updateSubtotal(product) {
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subtotalSpan = product.querySelector('.subtotal span');
+  const subtotal = parseFloat(price.textContent) * parseInt(quantity.value);
+
+  subtotalSpan.textContent = subtotal;
+return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const products = document.getElementsByClassName("product");
 
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += updateSubtotal(products[i]);
+} 
+const totalSpan = document.querySelector("#total-value span")
+totalSpan.textContent = total;
   // ITERATION 2
   //... your code goes here
 
